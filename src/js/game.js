e5d909af3689165b6005b111ae4312fbba3de7c5
sairoutine <sairoutine@gmail.com>
2018-01-02 23:11:35 +0900

    implement: game class

--- src/js/game.js
+++ src/js/game.js
@@ -1,6 +1,8 @@
 'use strict';
 var core = require('./hakurei').core;
 var util = require('./hakurei').util;
+var SceneTalk = require('./scene/talk');
+var SceneLoading = require('./scene/loading');
 
 var Game = function(canvas) {
 	core.apply(this, arguments);
@@ -9,6 +11,9 @@ util.inherit(Game, core);
 
 Game.prototype.init = function () {
 	core.prototype.init.apply(this, arguments);
+	this.addScene("loading", new SceneLoading(this));
+	this.addScene("title", new SceneTalk(this));
+	this.changeScene("loading");
 };
 
 module.exports = Game;
