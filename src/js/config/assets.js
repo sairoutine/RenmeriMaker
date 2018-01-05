4190cae309bb34d6c01b65b5b4e6585c2c8d966e
sairoutine <sairoutine@gmail.com>
2018-01-06 04:32:20 +0900

    add: assets

--- src/js/config/assets.js
+++ src/js/config/assets.js
@@ -3,8 +3,15 @@
 var AssetsConfig = {};
 
 AssetsConfig.images = {
-	// HowTo 画像
-	//"howto":            "./image/howto.png",
+	"renko_normal":    "./image/renko/normal1.png",
+	"renko_smile":     "./image/renko/smile1.png",
+	"renko_cry":       "./image/renko/cry1.png",
+	"renko_surprised": "./image/renko/surprised1.png",
+	"merry_normal":    "./image/merry/normal1.png",
+	"merry_smile":     "./image/merry/smile.png",
+	"merry_cry":       "./image/merry/cry1.png",
+	"merry_surprised": "./image/merry/surprised1.png",
+
 };
 
 AssetsConfig.sounds = {
@@ -17,14 +24,55 @@ AssetsConfig.sounds = {
 };
 
 AssetsConfig.bgms = {
-	/*
-	using_3rdeye: {
-		path: "./bgm/using_3rdeye.ogg",
+	nc13447: {
+		path: "./bgm/nc13447.mp3",
 		loopStart: 0*60 + 0 + 0.000,
 		//loopEnd: 1*60 + 47 + 0.027,
 		volume: 1.0,
 	},
-	*/
+	nc20349: {
+		path: "./bgm/nc20349.mp3",
+		loopStart: 0*60 + 0 + 0.000,
+		//loopEnd: 1*60 + 47 + 0.027,
+		volume: 1.0,
+	},
+	nc22928: {
+		path: "./bgm/nc22928.mp3",
+		loopStart: 0*60 + 0 + 0.000,
+		//loopEnd: 1*60 + 47 + 0.027,
+		volume: 1.0,
+	},
+	nc32144: {
+		path: "./bgm/nc32144.m4a",
+		loopStart: 0*60 + 0 + 0.000,
+		//loopEnd: 1*60 + 47 + 0.027,
+		volume: 1.0,
+	},
+	nc38444: {
+		path: "./bgm/nc38444.mp3",
+		loopStart: 0*60 + 0 + 0.000,
+		//loopEnd: 1*60 + 47 + 0.027,
+		volume: 1.0,
+	},
+	nc41740: {
+		path: "./bgm/nc41740.mp3",
+		loopStart: 0*60 + 0 + 0.000,
+		//loopEnd: 1*60 + 47 + 0.027,
+		volume: 1.0,
+	},
+
+	nc76949: {
+		path: "./bgm/nc76949.mp3",
+		loopStart: 0*60 + 0 + 0.000,
+		//loopEnd: 1*60 + 47 + 0.027,
+		volume: 1.0,
+	},
+
+
+
+
+
+
 };
 
 
