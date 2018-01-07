44765912412483b88c655307c041e52fd8f021cf
sairoutine <sairoutine@gmail.com>
2018-01-08 00:18:39 +0900

    update: add renmeri maker client codes

---
+++ src/js/config/assets.js
@@ -0,0 +1,88 @@
+'use strict';
+
+var AssetsConfig = {};
+
+AssetsConfig.images = {
+	"renko_normal":    "./image/renko/normal1.png",
+	"renko_smile":     "./image/renko/smile1.png",
+	"renko_cry":       "./image/renko/cry1.png",
+	"renko_surprised": "./image/renko/surprised1.png",
+	"renko_angry":     "./image/renko/angry1.png",
+	"merry_normal":    "./image/merry/normal1.png",
+	"merry_smile":     "./image/merry/smile.png",
+	"merry_cry":       "./image/merry/cry1.png",
+	"merry_surprised": "./image/merry/surprised1.png",
+	"merry_angry":     "./image/merry/angry1.png",
+
+	"nc138477": "./image/bg/nc138477.jpg",
+	"nc14162": "./image/bg/nc14162.jpg",
+	"nc4527": "./image/bg/nc4527.jpg",
+	"nc68222": "./image/bg/nc68222.jpg",
+	"nc72006": "./image/bg/nc72006.jpg",
+	"nc7951": "./image/bg/nc7951.jpg",
+	"nc95621": "./image/bg/nc95621.jpg",
+};
+
+AssetsConfig.sounds = {
+	/*
+	leaving_title: {
+		path: "./sound/leaving_title.ogg",
+		volume: 1.0,
+	},
+	*/
+};
+
+AssetsConfig.bgms = {
+	nc13447: {
+		path: "./bgm/nc13447.mp3",
+		loopStart: 0*60 + 0 + 0.000,
+		//loopEnd: 1*60 + 47 + 0.027,
+		volume: 1.0,
+	},
+	nc20349: {
+		path: "./bgm/nc20349.mp3",
+		loopStart: 0*60 + 0 + 0.000,
+		//loopEnd: 1*60 + 47 + 0.027,
+		volume: 1.0,
+	},
+	nc22928: {
+		path: "./bgm/nc22928.mp3",
+		loopStart: 0*60 + 0 + 0.000,
+		//loopEnd: 1*60 + 47 + 0.027,
+		volume: 1.0,
+	},
+	nc32144: {
+		path: "./bgm/nc32144.m4a",
+		loopStart: 0*60 + 0 + 0.000,
+		//loopEnd: 1*60 + 47 + 0.027,
+		volume: 1.0,
+	},
+	nc38444: {
+		path: "./bgm/nc38444.mp3",
+		loopStart: 0*60 + 0 + 0.000,
+		//loopEnd: 1*60 + 47 + 0.027,
+		volume: 1.0,
+	},
+	nc41740: {
+		path: "./bgm/nc41740.mp3",
+		loopStart: 0*60 + 0 + 0.000,
+		//loopEnd: 1*60 + 47 + 0.027,
+		volume: 1.0,
+	},
+
+	nc76949: {
+		path: "./bgm/nc76949.mp3",
+		loopStart: 0*60 + 0 + 0.000,
+		//loopEnd: 1*60 + 47 + 0.027,
+		volume: 1.0,
+	},
+
+
+
+
+
+
+};
+
+
+module.exports = AssetsConfig;
---
+++ src/js/main.js
@@ -0,0 +1,28 @@
+'use strict';
+var Game = require('./game');
+
+var game;
+
+window.onload = function() {
+	var mainCanvas = document.getElementById('mainCanvas');
+
+	var options = {};
+
+	game = new Game(mainCanvas, options);
+
+	game.setupEvents();
+
+	// reset keyboard binding
+	window.onkeydown = function(e) {};
+	window.onkeyup   = function(e) {};
+
+	game.init();
+	game.startRun();
+
+	window.game = game;
+};
+
+// for electron
+if(window.require) {
+	window.require('electron').webFrame.setVisualZoomLevelLimits(1,1); // unable to zoom
+}
