53a11dee464c6023b64efae4d443af60807b4293
sairoutine <sairoutine@gmail.com>
2018-01-07 16:50:16 +0900

    update: assets config

--- src/js/config/assets.js
+++ src/js/config/assets.js
@@ -7,10 +7,12 @@ AssetsConfig.images = {
 	"renko_smile":     "./image/renko/smile1.png",
 	"renko_cry":       "./image/renko/cry1.png",
 	"renko_surprised": "./image/renko/surprised1.png",
+	"renko_angry":     "./image/renko/angry1.png",
 	"merry_normal":    "./image/merry/normal1.png",
 	"merry_smile":     "./image/merry/smile.png",
 	"merry_cry":       "./image/merry/cry1.png",
 	"merry_surprised": "./image/merry/surprised1.png",
+	"merry_angry":     "./image/merry/angry1.png",
 
 	"nc138477": "./image/bg/nc138477.jpg",
 	"nc14162": "./image/bg/nc14162.jpg",
