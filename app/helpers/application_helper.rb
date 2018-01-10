3124c79f66af015b6789e0ead0e08ae6150897f7
sairoutine <sairoutine@gmail.com>
2018-01-11 05:42:04 +0900

    add: login methods in helper class

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+  # 現在ログイン中のユーザーを返す (いる場合)
+  def current_user
+    @current_user ||= User.find_by(id: session[:user_id])
+  end
+
+  # ユーザーがログインしていればtrue、その他ならfalseを返す
+  def logged_in?
+    !current_user.nil?
+  end
 end
