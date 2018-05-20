fee14d3f8518b318d1e1c9fe48a96f9b2e16e669
sairoutine <sairoutine@gmail.com>
2018-05-21 01:26:50 +0900

    update: migrate_db

--- script/migrate_db.go
+++ script/migrate_db.go
@@ -1,65 +1,60 @@
 package main
+
 import (
 	"github.com/jinzhu/gorm"
 	_ "github.com/jinzhu/gorm/dialects/mysql"
 	"time"
 )
 
-// User is.
-type User struct {
-    gorm.Model
-    Birthday     time.Time
-    Age          int
-    Name         string  `gorm:"size:255"` // Default size for string is 255, reset it with this tag
-    Num          int     `gorm:"AUTO_INCREMENT"`
+/*
+	User
+
 
     CreditCard        CreditCard      // One-To-One relationship (has one - use CreditCard's UserID as foreign key)
-    Emails            []Email         // One-To-Many relationship (has many - use Email's UserID as foreign key)
-
-    BillingAddress    Address         // One-To-One relationship (belongs to - use BillingAddressID as foreign key)
-    BillingAddressID  int
-
-    ShippingAddress   Address         // One-To-One relationship (belongs to - use ShippingAddressID as foreign key)
-    ShippingAddressID int
-
     IgnoreMe          int `gorm:"-"`   // Ignore this field
     Languages         []Language `gorm:"many2many:user_languages;"` // Many-To-Many relationship, 'user_languages' is join table
-}
 
-// Email is.
-type Email struct {
-    ID      int
-    UserID  int     `gorm:"index"` // Foreign key (belongs to), tag `index` will create index for this column
+	Email
+
+
     Email   string  `gorm:"type:varchar(100);unique_index"` // `type` set sql type, `unique_index` will create unique index for this column
     Subscribed bool
+
+*/
+
+// User is.
+type User struct {
+	ID                       uint64 `gorm:"AUTO_INCREMENT"`
+	DispName                 string `gorm:"size:255"`
+	LastShowNotificationDate time.Time
+	Novels                   []Novel // One-To-Many relationship (has many - use Novel's UserID as foreign key)
 }
 
-// Address is.
-type Address struct {
-    ID       int
-    Address1 string         `gorm:"not null;unique"` // Set field as not nullable and unique
-    Address2 string         `gorm:"type:varchar(100);unique"`
-    Post     string `gorm:"not null"`
+// Novel is.
+type Novel struct {
+	ID           uint64  `gorm:"AUTO_INCREMENT"`
+	UserID       uint64  `gorm:"index"`
+	Title        string  `gorm:"size:255"`
+	Introduction string  `gorm:"size:255"`
+	Script       string  `gorm:"size:255"`
+	Emojis       []Emoji // One-To-Many relationship (has many - use Emoji's UserID as foreign key)
 }
 
-// Language is.
-type Language struct {
-    ID   int
-    Name string `gorm:"index:idx_name_code"` // Create index with name, and will create combined index if find other fields defined same name
-    Code string `gorm:"index:idx_name_code"` // `unique_index` also works
-}
-
-// CreditCard is.
-type CreditCard struct {
-    gorm.Model
-    UserID  uint
-    Number  string
+// Emoji is.
+// unique: (novel_id, type)
+type Emoji struct {
+	ID uint64 `gorm:"AUTO_INCREMENT"`
+	// multiple unique key
+	NovelID uint64 `gorm:"         unique_index:idx_novel_id_type"`
+	Type    string `gorm:"size:255;unique_index:idx_novel_id_type"`
+	Count   uint
+	Script  string `gorm:"size:255"`
 }
 
 func main() {
 	db, err := gorm.Open("mysql", "root@tcp(localhost:3306)/renmeri_maker?charset=utf8&parseTime=True&loc=Local")
 	if err != nil {
-		panic(err.Error());
+		panic(err.Error())
 	}
 
 	defer db.Close()
@@ -68,8 +63,6 @@ func main() {
 
 	db.AutoMigrate(
 		&User{},
-		&Email{},
-		&Address{},
-		&Language{},
-		&CreditCard{})
+		&Novel{},
+		&Emoji{})
 }
