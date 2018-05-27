d3fa45c252b4f1aa20b939ecd7eb10ec31d2571b
sairoutine <sairoutine@gmail.com>
2018-05-28 02:06:13 +0900

    update: novel edit controller

--- app/controller/novel/novel.go
+++ app/controller/novel/novel.go
@@ -1,7 +1,11 @@
 package novel
 
 import (
+	"github.com/gin-contrib/sessions"
 	"github.com/gin-gonic/gin"
+	"github.com/jinzhu/gorm"
+	"github.com/sairoutine/RenmeriMaker/app/model"
+	"github.com/sairoutine/RenmeriMaker/app/util"
 	"net/http"
 )
 
@@ -13,7 +17,32 @@ func New(c *gin.Context) {
 
 }
 func Edit(c *gin.Context) {
+	db := c.MustGet("DB").(*gorm.DB)
+	id := c.Param("id")
 
+	novel := model.Novel{}
+	recordNotFound := db.Where(&model.Novel{ID: util.String2Uint64(id)}).First(&novel).RecordNotFound()
+
+	// 存在しないノベルならエラー
+	if recordNotFound {
+		util.RenderNotFound(c)
+		return
+	}
+
+	// ログイン情報を取得
+	session := sessions.Default(c)
+	loginUserId := session.Get("user_id").(uint64)
+
+	// 自分が作ったノベルでなければエラー
+	if novel.UserID != loginUserId {
+		util.RenderForbidden(c)
+		return
+	}
+
+	c.HTML(http.StatusOK, "novel/edit.tmpl", gin.H{
+		"isNew": 0, // false
+		"id":    id,
+	})
 }
 
 func Show(c *gin.Context) {
