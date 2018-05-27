fc6257c22c69d72ce2364f15b8ef256625688a78
sairoutine <sairoutine@gmail.com>
2018-05-28 02:03:32 +0900

    update: add comment and 403 error renderer function

--- app/util/render.go
+++ app/util/render.go
@@ -5,6 +5,12 @@ import (
 	"net/http"
 )
 
+// 404 エラー
 func RenderNotFound(c *gin.Context) {
 	c.HTML(http.StatusNotFound, "error/404.tmpl", nil)
+}
+
+// 403 エラー
+func RenderForbidden(c *gin.Context) {
+	c.HTML(http.StatusForbidden, "error/403.tmpl", nil)
 }
