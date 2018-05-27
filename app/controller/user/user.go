106d45be095f83f865e9c2da69181a7b4a2c5ed3
sairoutine <sairoutine@gmail.com>
2018-05-27 21:05:26 +0900

    update: show user

--- app/controller/user/user.go
+++ app/controller/user/user.go
@@ -4,6 +4,8 @@ import (
 	"github.com/gin-gonic/gin"
 	"github.com/jinzhu/gorm"
 	model "github.com/sairoutine/RenmeriMaker/app/model"
+	"github.com/sairoutine/RenmeriMaker/app/util"
+	"net/http"
 )
 
 func Index(c *gin.Context) {
@@ -11,11 +13,17 @@ func Index(c *gin.Context) {
 }
 func Show(c *gin.Context) {
 	db := c.MustGet("DB").(*gorm.DB)
+	id := c.Param("id")
 
-	record := model.User{}
-	db.First(&record)
-	c.JSON(200, gin.H{
-		"ID":   record.ID,
-		"Name": record.DispName,
-	})
+	user := model.User{}
+	recordNotFound := db.Where(&model.User{ID: util.String2Uint64(id)}).First(&user).RecordNotFound()
+
+	if !recordNotFound {
+		c.HTML(http.StatusOK, "user/show.html", gin.H{
+			"ID":   user.ID,
+			"Name": user.DispName,
+		})
+	} else {
+		util.RenderNotFound(c)
+	}
 }
