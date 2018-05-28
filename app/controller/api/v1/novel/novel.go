ddb6bffed4ce84a4194501859ec7a1279c65818d
sairoutine <sairoutine@gmail.com>
2018-05-29 01:30:13 +0900

    implement: novel edit api

--- app/controller/api/v1/novel/novel.go
+++ app/controller/api/v1/novel/novel.go
@@ -40,5 +40,44 @@ func Create(c *gin.Context) {
 }
 
 func Update(c *gin.Context) {
+	db := c.MustGet("DB").(*gorm.DB)
+	id := c.Param("id")
 
+	// POST データ取得
+	c.Request.ParseForm()
+	script := c.Request.Form["script"][0]
+
+	novel := model.Novel{}
+	recordNotFound := db.Where(&model.Novel{ID: util.String2Uint64(id)}).First(&novel).RecordNotFound()
+
+	// 存在しないノベルならエラー
+	if recordNotFound {
+		util.RenderNotFound(c)
+		return
+	}
+
+	// ログイン情報を取得
+	session := sessions.Default(c)
+	loginUserId, ok := session.Get("user_id").(uint64)
+
+	// ログインしていなければエラー
+	if !ok {
+		util.RenderForbidden(c)
+		return
+	}
+
+	// 自分が作ったノベルでなければエラー
+	if novel.UserID != loginUserId {
+		util.RenderForbidden(c)
+		return
+	}
+
+	novel.Script = script
+
+	// 新規登録
+	db.Save(&novel)
+
+	c.JSON(http.StatusOK, gin.H{
+		"id": novel.ID,
+	})
 }
