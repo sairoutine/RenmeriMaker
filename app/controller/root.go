e59fa34388f72a4afbcc8016746054c1ec16b867
sairoutine <sairoutine@gmail.com>
2018-05-29 03:10:50 +0900

    update: show novel list in index page

--- app/controller/root.go
+++ app/controller/root.go
@@ -2,9 +2,18 @@ package controller
 
 import (
 	"github.com/gin-gonic/gin"
+	"github.com/jinzhu/gorm"
+	"github.com/sairoutine/RenmeriMaker/app/model"
 	"net/http"
 )
 
 func Index(c *gin.Context) {
-	c.HTML(http.StatusOK, "root/index.tmpl", gin.H{})
+	db := c.MustGet("DB").(*gorm.DB)
+
+	novels := []model.Novel{}
+	db.Find(&novels)
+
+	c.HTML(http.StatusOK, "root/index.tmpl", gin.H{
+		"novels": novels,
+	})
 }
