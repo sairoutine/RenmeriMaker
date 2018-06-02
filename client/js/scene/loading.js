1d56d671bc44d0f3d8e0e01cc4fc4f81c11fea8e
sairoutine <sairoutine@gmail.com>
2018-06-03 01:02:37 +0900

    update: refactor loading scene

--- client/js/scene/loading.js
+++ client/js/scene/loading.js
@@ -2,10 +2,9 @@
 
 // ローディングシーン
 
-var base_scene = require('../hakurei').scene.base;
+var base_scene = require('../hakurei').scene.loading;
 var Util = require('../hakurei').util;
 var AssetsConfig = require('../config/assets');
-var CONSTANT = require('../constant');
 
 var SceneLoading = function(core) {
 	base_scene.apply(this, arguments);
@@ -13,48 +12,10 @@ var SceneLoading = function(core) {
 Util.inherit(SceneLoading, base_scene);
 
 SceneLoading.prototype.init = function() {
-	base_scene.prototype.init.apply(this, arguments);
+	base_scene.prototype.init.apply(this, [AssetsConfig, "talk"]);
 
-	// ゲームで使用する画像一覧
-	for (var key in AssetsConfig.images) {
-		var image_conf = AssetsConfig.images[key];
-		if (typeof image_conf === "string") {
-			this.core.image_loader.loadImage(key, image_conf);
-		}
-		else {
-			this.core.image_loader.loadImage(key, image_conf.image, image_conf.scale_width, image_conf.scale_height);
-		}
-	}
-
-	// ゲームで使用するSE一覧
-	for (var key2 in AssetsConfig.sounds) {
-		var conf2 = AssetsConfig.sounds[key2];
-
-		// デバッグ用ミュート
-		var volume2 = CONSTANT.DEBUG.SOUND_OFF ? 0 : conf2.volume;
-
-		this.core.audio_loader.loadSound(key2, conf2.path, volume2);
-	}
-
-	// ゲームで使用するBGM一覧
-	for (var key3 in AssetsConfig.bgms) {
-		var conf3 = AssetsConfig.bgms[key3];
-		// デバッグ用ミュート
-		var volume3 = CONSTANT.DEBUG.SOUND_OFF ? 0 : conf3.volume;
-
-		this.core.audio_loader.loadBGM(key3, conf3.path, volume3, conf3.loopStart, conf3.loopEnd);
-	}
 };
 
-SceneLoading.prototype.beforeDraw = function() {
-	base_scene.prototype.beforeDraw.apply(this, arguments);
-
-	//if (this.core.image_loader.isAllLoaded() && this.core.audio_loader.isAllLoaded() && this.core.font_loader.isAllLoaded()) {
-	if (this.core.image_loader.isAllLoaded() && this.core.audio_loader.isAllLoaded()) {
-		// 本番
-		this.core.scene_manager.changeScene("talk");
-	}
-};
 SceneLoading.prototype.draw = function(){
 	base_scene.prototype.draw.apply(this, arguments);
 	var ctx = this.core.ctx;
@@ -98,10 +59,5 @@ SceneLoading.prototype.draw = function(){
 	ctx.restore();
 };
 
-
-SceneLoading.prototype.progress = function(){
-	var progress = (this.core.audio_loader.progress() + this.core.image_loader.progress() + this.core.font_loader.progress()) / 3;
-	return progress;
-};
 
 module.exports = SceneLoading;
