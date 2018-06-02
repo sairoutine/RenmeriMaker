2e719f5ac5816a088e6e6af53ae04c84a83c593d
sairoutine <sairoutine@gmail.com>
2018-06-03 01:02:16 +0900

    update: config

--- client/js/config/assets.js
+++ client/js/config/assets.js
@@ -14,13 +14,34 @@ AssetsConfig.images = {
 	"merry_surprised": "/image/merry/surprised1.png",
 	"merry_angry":     "/image/merry/angry1.png",
 
-	"nc138477": "/image/bg/nc138477.jpg",
-	"nc14162": "/image/bg/nc14162.jpg",
-	"nc4527": "/image/bg/nc4527.jpg",
-	"nc68222": "/image/bg/nc68222.jpg",
-	"nc72006": "/image/bg/nc72006.jpg",
-	"nc7951": "/image/bg/nc7951.jpg",
-	"nc95621": "/image/bg/nc95621.jpg",
+	"nc138477": {
+		name: "森の中",
+		path: "/image/bg/nc138477.jpg",
+	},
+	"nc14162": {
+		name: "夜の海岸",
+		path: "/image/bg/nc14162.jpg",
+	},
+	"nc4527": {
+		name: "道路",
+		path: "/image/bg/nc4527.jpg",
+	},
+	"nc68222": {
+		name: "校門前",
+		path: "/image/bg/nc68222.jpg",
+	},
+	"nc72006": {
+		name: "夜の森林",
+		path: "/image/bg/nc72006.jpg",
+	},
+	"nc7951": {
+		name: "学内",
+		path: "/image/bg/nc7951.jpg",
+	},
+	"nc95621": {
+		name: "お店",
+		path: "/image/bg/nc95621.jpg",
+	},
 };
 
 AssetsConfig.sounds = {
@@ -34,36 +55,42 @@ AssetsConfig.sounds = {
 
 AssetsConfig.bgms = {
 	nc13447: {
+		name: "しんみり",
 		path: "/bgm/nc13447.mp3",
 		loopStart: 0*60 + 0 + 0.000,
 		//loopEnd: 1*60 + 47 + 0.027,
 		volume: 1.0,
 	},
 	nc20349: {
+		name: "やる気がない",
 		path: "/bgm/nc20349.mp3",
 		loopStart: 0*60 + 0 + 0.000,
 		//loopEnd: 1*60 + 47 + 0.027,
 		volume: 1.0,
 	},
 	nc22928: {
+		name: "ナチュラル",
 		path: "/bgm/nc22928.mp3",
 		loopStart: 0*60 + 0 + 0.000,
 		//loopEnd: 1*60 + 47 + 0.027,
 		volume: 1.0,
 	},
 	nc32144: {
+		name: "おしゃれ",
 		path: "/bgm/nc32144.m4a",
 		loopStart: 0*60 + 0 + 0.000,
 		//loopEnd: 1*60 + 47 + 0.027,
 		volume: 1.0,
 	},
 	nc38444: {
+		name: "ほのぼの",
 		path: "/bgm/nc38444.mp3",
 		loopStart: 0*60 + 0 + 0.000,
 		//loopEnd: 1*60 + 47 + 0.027,
 		volume: 1.0,
 	},
 	nc41740: {
+		name: "キラキラ",
 		path: "/bgm/nc41740.mp3",
 		loopStart: 0*60 + 0 + 0.000,
 		//loopEnd: 1*60 + 47 + 0.027,
@@ -71,6 +98,7 @@ AssetsConfig.bgms = {
 	},
 
 	nc76949: {
+		name: "和風",
 		path: "/bgm/nc76949.mp3",
 		loopStart: 0*60 + 0 + 0.000,
 		//loopEnd: 1*60 + 47 + 0.027,
