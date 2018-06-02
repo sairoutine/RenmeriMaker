4da5c2094e469d30d5c31df237992506ac41ef00
sairoutine <sairoutine@gmail.com>
2018-06-03 02:10:55 +0900

    update: refactor main.js

--- client/js/main.js
+++ client/js/main.js
@@ -1,21 +1,8 @@
 'use strict';
-var Game = require('./game/game');
-
+var runGame = require('./run_game');
 window.onload = function() {
 	var mainCanvas = document.getElementById('mainCanvas');
 
-	// サーバーサイドからのデータ
-	var options = window.config;
-
-	var game = new Game(mainCanvas, options);
-
-	game.setupEvents();
-
-	// reset keyboard binding
-	window.onkeydown = function(e) {};
-	window.onkeyup   = function(e) {};
-
-	game.init();
-	game.startRun();
-
+	runGame(mainCanvas);
 };
+
