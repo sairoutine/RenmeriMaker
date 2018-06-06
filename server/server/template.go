76b381abfee6f5f6746d45a7c18a8915c7193d8c
sairoutine <sairoutine@gmail.com>
2018-06-07 00:04:21 +0900

    add: template middleware

---
+++ server/server/template.go
@@ -0,0 +1,38 @@
+package server
+
+import (
+	"github.com/gin-contrib/multitemplate"
+	"html/template"
+	"path/filepath"
+)
+
+func (s *Server) SetupTemplate() {
+	r := s.Engine
+
+	templatesDir := "../template"
+	r.HTMLRender = loadTemplates(
+		templatesDir,
+		filepath.Join(templatesDir, "include"),
+		filepath.Join(templatesDir, "layout.tmpl"))
+}
+
+func loadTemplates(templatesDir, includeDir, layoutFile string) multitemplate.Renderer {
+	r := multitemplate.NewRenderer()
+
+	templateFiles, err := filepath.Glob(templatesDir + "/**/*")
+	if err != nil {
+		panic(err.Error())
+	}
+
+	includes, err := filepath.Glob(filepath.Join(includeDir, "*"))
+	if err != nil {
+		panic(err.Error())
+	}
+
+	for _, temp := range templateFiles {
+		files := append([]string{layoutFile, temp}, includes...)
+		rel, _ := filepath.Rel(templatesDir, temp)
+		r.Add(rel, template.Must(template.ParseFiles(files...)))
+	}
+	return r
+}
